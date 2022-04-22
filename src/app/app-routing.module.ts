665393731f6f5f6b0f0aa7f823454091d4c0feb3
rasec1106 <elch.herrera@gmail.com>
2022-04-22 17:57:41 -0500

    Add routing example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
+import { TaskPageComponent } from './modules/task/page/task-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from '@modules/auth/page/login-page.component';
 
-const routes: Routes = [{
-  path:'',
-  component: LoginPageComponent
-}];
+const routes: Routes = [
+  {
+    path:'',
+    component: LoginPageComponent
+  },
+  {
+    path:'task',
+    component: TaskPageComponent
+  },
+  {
+    path: '**', // Routes that are not mapped
+    // component: LoginPageComponent,
+    redirectTo: '/task' // to redirect the page to the specified url
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
